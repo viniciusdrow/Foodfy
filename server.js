3f5e37324fa1cf916431f705fe24138a5f43e2dc
viniciusdrow <vinisb2@hotmail.com>
2021-04-05 10:34:24 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+const recipes_list = require('./data')
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server,
+    noCache: true
+})
+
+server.get("/", function(req, res) {
+    return res.render("home")
+})
+ 
+
+server.get("/receitas", function(req, res) {
+    const id = req.params.id
+
+    const receitas = recipes_list.find(function(receitas){
+        return receitas.id == id
+    }) 
+    if(!receitas) {
+        return res.send("Recipes not found!")
+    }
+
+    return res.render("receitas", { items: receitas })
+   
+})
+
+server.get("/recipes", function(req, res) {
+    return res.render("recipes", { items: recipes_list})
+
+})
+
+server.get("/recipes/:id", function (req, res) {
+    const id = req.params.id;
+    const recipes = recipes_list[id]; // Array de receitas carregadas do data.js
+  
+
+return res.render('recipes', { recipes});
+
+})
+
+server.get("/sobre", function(req, res) {
+    return res.render("about")
+})
+
+server.listen(5003, function() {
+    console.log("server is running")
+})
+
+
+
+
+
+
